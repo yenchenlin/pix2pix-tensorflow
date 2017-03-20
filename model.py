0aefb19025df5678ec93c0890fe5ce548527e888
Gijs Molenaar <gijs@pythonic.nl>
2017-03-20 11:18:14 +0200

    order of arguments change for tf.concat

--- model.py
+++ model.py
@@ -77,8 +77,8 @@ class pix2pix(object):
 
         self.fake_B = self.generator(self.real_A)
 
-        self.real_AB = tf.concat(3, [self.real_A, self.real_B])
-        self.fake_AB = tf.concat(3, [self.real_A, self.fake_B])
+        self.real_AB = tf.concat([self.real_A, self.real_B], 3)
+        self.fake_AB = tf.concat([self.real_A, self.fake_B], 3)
         self.D, self.D_logits = self.discriminator(self.real_AB, reuse=False)
         self.D_, self.D_logits_ = self.discriminator(self.fake_AB, reuse=True)
 
@@ -237,43 +237,43 @@ class pix2pix(object):
         self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e8),
             [self.batch_size, s128, s128, self.gf_dim*8], name='g_d1', with_w=True)
         d1 = tf.nn.dropout(self.g_bn_d1(self.d1), 0.5)
-        d1 = tf.concat(3, [d1, e7])
+        d1 = tf.concat([d1, e7], 3)
         # d1 is (2 x 2 x self.gf_dim*8*2)
 
         self.d2, self.d2_w, self.d2_b = deconv2d(tf.nn.relu(d1),
             [self.batch_size, s64, s64, self.gf_dim*8], name='g_d2', with_w=True)
         d2 = tf.nn.dropout(self.g_bn_d2(self.d2), 0.5)
-        d2 = tf.concat(3, [d2, e6])
+        d2 = tf.concat([d2, e6], 3)
         # d2 is (4 x 4 x self.gf_dim*8*2)
 
         self.d3, self.d3_w, self.d3_b = deconv2d(tf.nn.relu(d2),
             [self.batch_size, s32, s32, self.gf_dim*8], name='g_d3', with_w=True)
         d3 = tf.nn.dropout(self.g_bn_d3(self.d3), 0.5)
-        d3 = tf.concat(3, [d3, e5])
+        d3 = tf.concat([d3, e5], 3)
         # d3 is (8 x 8 x self.gf_dim*8*2)
 
         self.d4, self.d4_w, self.d4_b = deconv2d(tf.nn.relu(d3),
             [self.batch_size, s16, s16, self.gf_dim*8], name='g_d4', with_w=True)
         d4 = self.g_bn_d4(self.d4)
-        d4 = tf.concat(3, [d4, e4])
+        d4 = tf.concat([d4, e4], 3)
         # d4 is (16 x 16 x self.gf_dim*8*2)
 
         self.d5, self.d5_w, self.d5_b = deconv2d(tf.nn.relu(d4),
             [self.batch_size, s8, s8, self.gf_dim*4], name='g_d5', with_w=True)
         d5 = self.g_bn_d5(self.d5)
-        d5 = tf.concat(3, [d5, e3])
+        d5 = tf.concat([d5, e3], 3)
         # d5 is (32 x 32 x self.gf_dim*4*2)
 
         self.d6, self.d6_w, self.d6_b = deconv2d(tf.nn.relu(d5),
             [self.batch_size, s4, s4, self.gf_dim*2], name='g_d6', with_w=True)
         d6 = self.g_bn_d6(self.d6)
-        d6 = tf.concat(3, [d6, e2])
+        d6 = tf.concat([d6, e2], 3)
         # d6 is (64 x 64 x self.gf_dim*2*2)
 
         self.d7, self.d7_w, self.d7_b = deconv2d(tf.nn.relu(d6),
             [self.batch_size, s2, s2, self.gf_dim], name='g_d7', with_w=True)
         d7 = self.g_bn_d7(self.d7)
-        d7 = tf.concat(3, [d7, e1])
+        d7 = tf.concat([d7, e1], 3)
         # d7 is (128 x 128 x self.gf_dim*1*2)
 
         self.d8, self.d8_w, self.d8_b = deconv2d(tf.nn.relu(d7),
@@ -309,43 +309,43 @@ class pix2pix(object):
         self.d1, self.d1_w, self.d1_b = deconv2d(tf.nn.relu(e8),
             [self.batch_size, s128, s128, self.gf_dim*8], name='g_d1', with_w=True)
         d1 = tf.nn.dropout(self.g_bn_d1(self.d1), 0.5)
-        d1 = tf.concat(3, [d1, e7])
+        d1 = tf.concat([d1, e7], 3)
         # d1 is (2 x 2 x self.gf_dim*8*2)
 
         self.d2, self.d2_w, self.d2_b = deconv2d(tf.nn.relu(d1),
             [self.batch_size, s64, s64, self.gf_dim*8], name='g_d2', with_w=True)
         d2 = tf.nn.dropout(self.g_bn_d2(self.d2), 0.5)
-        d2 = tf.concat(3, [d2, e6])
+        d2 = tf.concat([d2, e6], 3)
         # d2 is (4 x 4 x self.gf_dim*8*2)
 
         self.d3, self.d3_w, self.d3_b = deconv2d(tf.nn.relu(d2),
             [self.batch_size, s32, s32, self.gf_dim*8], name='g_d3', with_w=True)
         d3 = tf.nn.dropout(self.g_bn_d3(self.d3), 0.5)
-        d3 = tf.concat(3, [d3, e5])
+        d3 = tf.concat([d3, e5], 3)
         # d3 is (8 x 8 x self.gf_dim*8*2)
 
         self.d4, self.d4_w, self.d4_b = deconv2d(tf.nn.relu(d3),
             [self.batch_size, s16, s16, self.gf_dim*8], name='g_d4', with_w=True)
         d4 = self.g_bn_d4(self.d4)
-        d4 = tf.concat(3, [d4, e4])
+        d4 = tf.concat([d4, e4], 3)
         # d4 is (16 x 16 x self.gf_dim*8*2)
 
         self.d5, self.d5_w, self.d5_b = deconv2d(tf.nn.relu(d4),
             [self.batch_size, s8, s8, self.gf_dim*4], name='g_d5', with_w=True)
         d5 = self.g_bn_d5(self.d5)
-        d5 = tf.concat(3, [d5, e3])
+        d5 = tf.concat([d5, e3], 3)
         # d5 is (32 x 32 x self.gf_dim*4*2)
 
         self.d6, self.d6_w, self.d6_b = deconv2d(tf.nn.relu(d5),
             [self.batch_size, s4, s4, self.gf_dim*2], name='g_d6', with_w=True)
         d6 = self.g_bn_d6(self.d6)
-        d6 = tf.concat(3, [d6, e2])
+        d6 = tf.concat([d6, e2], 3)
         # d6 is (64 x 64 x self.gf_dim*2*2)
 
         self.d7, self.d7_w, self.d7_b = deconv2d(tf.nn.relu(d6),
             [self.batch_size, s2, s2, self.gf_dim], name='g_d7', with_w=True)
         d7 = self.g_bn_d7(self.d7)
-        d7 = tf.concat(3, [d7, e1])
+        d7 = tf.concat([d7, e1], 3)
         # d7 is (128 x 128 x self.gf_dim*1*2)
 
         self.d8, self.d8_w, self.d8_b = deconv2d(tf.nn.relu(d7),
