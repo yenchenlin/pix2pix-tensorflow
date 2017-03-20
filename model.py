971742b803b385a8532b26530c191e29759c748a
Gijs Molenaar <gijs@pythonic.nl>
2017-03-20 11:59:57 +0200

    tf.train.SummaryWriter renamed

--- model.py
+++ model.py
@@ -142,7 +142,7 @@ class pix2pix(object):
         self.g_sum = tf.summary.merge([self.d__sum,
             self.fake_B_sum, self.d_loss_fake_sum, self.g_loss_sum])
         self.d_sum = tf.summary.merge([self.d_sum, self.d_loss_real_sum, self.d_loss_sum])
-        self.writer = tf.train.SummaryWriter("./logs", self.sess.graph)
+        self.writer = tf.summary.FileWriter("./logs", self.sess.graph)
 
         counter = 1
         start_time = time.time()
